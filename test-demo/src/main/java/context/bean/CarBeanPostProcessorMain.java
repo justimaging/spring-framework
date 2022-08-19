8987817c83780cabe8075379eda50d02c5233ccd
朱淑献 <zhushuxian@digitalgd.com.cn>
2022-08-19 16:42:12 +0800

    [+]事件监听、后置处理器

---
+++ test-demo/src/main/java/context/bean/CarBeanPostProcessorMain.java
@@ -0,0 +1,26 @@
+package context.bean;
+
+import base.factory.bean.Car;
+import domain.ComplexBook;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * bean 后处理器 启动类
+ * @author JingQ at 2019-06-17
+ */
+public class CarBeanPostProcessorMain {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("factory.bean/bean-post-processor.xml");
+		Car car = (Car) context.getBean("car");
+		ComplexBook book = (ComplexBook) context.getBean("book");
+		System.out.println(car);
+		System.out.println(book);
+
+		// 实测，ClassPathXmlApplicationContext 无法在应用中硬编码，只能从配置文件中注册，延迟加载 bean 或者其它 BeanFactory 或许会支持硬编码
+//		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("factory.bean/bean-post-processor.xml");
+//		CarBeanPostProcessor processor = new CarBeanPostProcessor();
+//		context.getBeanFactory().addBeanPostProcessor(processor);
+	}
+}
